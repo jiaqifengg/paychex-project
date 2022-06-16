43f55ce5e069aee3c039a71f4def42c132318329
Jia Qi Feng <jiaqifen@buffalo.edu>
2022-06-16 17:40:57 -0400

    successfully initialized connection to the pgadmin4

--- backend/base.py
+++ backend/base.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, render_template_string
+from database.dbHelper import dbHelper
+import json
+from flask import Flask, jsonify
 
-api = Flask(__name__)
 
-@api.route('/profile')
-def my_profile():
+app = Flask(__name__)
+db = dbHelper()
+
+@app.route('/', methods=['GET'])
+def index():
+    words = db.test()
     response_body = {
-        "name": "Nagato",
-        "about" :"Hello! I'm a full stack developer that loves python and javascript"
+        "name": "Kelly",
+        "about" :"Hello! I'm a full stack developer that loves python and javascript",
+        "conn": words
     }
+    return (response_body)  
 
-    return response_body
+@app.route('/test', methods=['GET'])
+def tester():
+    words = db.test()
+    response_body = {
+        "name": "Kelly",
+        "about" :"Second Test",
+        "conn": words
+    }
+    return (response_body)  
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
--- backend/database/dbHelper.py
+++ backend/database/dbHelper.py
@@ -1,2 +1,25 @@
 import psycopg2
 import hashlib
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+class dbHelper():
+    def __init__(self):
+        self.connection = psycopg2.connect(user='postgres',
+        password='0107kf', host='localhost',dbname='paychexDB')
+        self.cursor = self.connection.cursor()
+
+        create_employees_table = """CREATE TABLE IF NOT EXISTS employees(
+                                    empid CHAR(8) PRIMARY KEY,
+                                    username VARCHAR(18) NOT NULL UNIQUE,
+                                    f_name VARCHAR(25) NOT NULL,
+                                    l_name VARCHAR(16) NOT NULL,
+                                    password VARCHAR(50) NOT NULL
+                                );"""
+
+        self.cursor.execute(create_employees_table)
+        self.connection.commit()
+
+    def test(self):
+        return "testing connection"
