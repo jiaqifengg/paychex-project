c66e97f6e1dac6da2871654734698c8e0b09a27f
Jia Qi Feng <jiaqifen@buffalo.edu>
2022-06-18 23:35:27 -0400

    added try except for datbaase connections

--- backend/base.py
+++ backend/base.py
@@ -15,47 +15,49 @@ def index():
     response_body = {
         "name": "Kelly",
         "about" :"Hello! I'm a full stack developer that loves python and javascript",
-        "conn": words
+        "conn": words,
+        "status": db.db_status
     }
     return (response_body)  
 
-@app.route('/test', methods=['GET'])
-def tester():
-    words = db.test()
-    response_body = {
-        "name": "Kelly",
-        "about" :"Second Test",
-        "conn": words
-    }
-    return (response_body)  
-@app.route('/register', methods=['POST', 'GET'])
-def register():
-    salt = os.urandom(32)
-    input_username = request.args.get('username')
-    input_password = request.args.get('password')
-    f_name = request.args.get('First Name')
-    l_name = request.args.get('Last Name')
+# @app.route('/test', methods=['GET'])
+# def tester():
+#     words = db.test()
+#     response_body = {
+#         "name": "Kelly",
+#         "about" :"Second Test",
+#         "conn": words
+#     }
+#     return (response_body)  
 
-    hashed_pw = hashlib.pbkdf2_hmac('sha256', input_password.encode('utf-8'), salt , 100000, dklen=128)
+# @app.route('/register', methods=['POST', 'GET'])
+# def register():
+#     salt = os.urandom(32)
+#     input_username = request.args.get('username')
+#     input_password = request.args.get('password')
+#     f_name = request.args.get('First Name')
+#     l_name = request.args.get('Last Name')
 
-    db.addEmployee(f_name, l_name, )
+#     hashed_pw = hashlib.pbkdf2_hmac('sha256', input_password.encode('utf-8'), salt , 100000, dklen=128)
+
+#     db.addEmployee(f_name, l_name, )
     
-@app.route('/auth', methods=['GET','POST'])
-def login():
-    # get arguments from post request
-    input_username = request.args.get('username')
-    input_password = request.args.get('password')
+# @app.route('/auth', methods=['GET','POST'])
+# def login():
+#     # get arguments from post request
+#     input_username = request.args.get('username')
+#     input_password = request.args.get('password')
 
-    # get salt 
-    stored_salt = b''
-    stored_key = b''
+#     # get salt 
+#     stored_salt = b''
+#     stored_key = b''
 
-    new_password = hashlib.pbkdf2_hmac('sha256', input_password.encode('utf-8'), stored_salt, 100000, dklen=128)
+#     new_password = hashlib.pbkdf2_hmac('sha256', input_password.encode('utf-8'), stored_salt, 100000, dklen=128)
 
-    if stored_key == new_password:
-        return {"msg": 200}
-    else:
-        return {"msg": 403}
+#     if stored_key == new_password:
+#         return {"msg": 200}
+#     else:
+#         return {"msg": 403}
 
 if __name__ == "__main__":
     app.debug = True
--- backend/database/dbHelper.py
+++ backend/database/dbHelper.py
@@ -6,21 +6,31 @@ import os
 
 class dbHelper():
     def __init__(self):
-        self.connection = psycopg2.connect(user='postgres',
-        password='0107kf', host='localhost',dbname='paychexDB')
-        self.cursor = self.connection.cursor()
+        self.db_status = 200
+        try:
+            self.connection = psycopg2.connect(user='postgres',
+            password='0107kf', host='localhost',dbname='paychexDB')
+            self.cursor = self.connection.cursor()
 
-        create_employees_table = """CREATE TABLE IF NOT EXISTS employees(
-                                    empid CHAR(8) PRIMARY KEY,
-                                    username VARCHAR(18) NOT NULL UNIQUE,
-                                    f_name VARCHAR(25) NOT NULL,
-                                    l_name VARCHAR(16) NOT NULL,
-                                    password VARCHAR(160) NOT NULL,
-                                    salt VARCHAR(160) NOT NULL
-                                );"""
+            create_employees_table = """CREATE TABLE IF NOT EXISTS employees(
+                                        empid CHAR(8) PRIMARY KEY,
+                                        username VARCHAR(18) NOT NULL UNIQUE,
+                                        f_name VARCHAR(25) NOT NULL,
+                                        l_name VARCHAR(16) NOT NULL,
+                                        hash_pw VARCHAR(160) NOT NULL,
+                                        salt VARCHAR(160) NOT NULL
+                                    );"""
 
-        self.cursor.execute(create_employees_table)
-        self.connection.commit()
+            self.cursor.execute(create_employees_table)
+
+            create_timesheet_table = """CREATE TABLE IF NOT EXISTS timesheets(
+                                        employee_id CHAR(8),
+                                        
+                                    );"""
+            self.connection.commit()
+            self.db_status = 200
+        except:
+            self.db_status = 500
 
     def test(self):
         return "testing connection"
