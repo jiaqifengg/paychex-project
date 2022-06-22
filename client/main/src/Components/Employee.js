9a2b4cae1a9c89b254dcf6c9a21f4fb813de2387
Jia Qi Feng <jiaqifen@buffalo.edu>
2022-06-22 17:17:48 -0400

    styled side nav bar for portal page

--- client/main/src/Components/Employee.js
+++ client/main/src/Components/Employee.js
@@ -1,28 +1,45 @@
 import React from 'react';
 import "../styles/standard.css";
+import Logout from "../icons/logout.png";
 export default class Employee extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      shiftActive: false
+      shiftActive: false,
+      timesheet: true
     };
   }
 
   componentDidMount(){
-    // fetch("http://127.0.0.1:5000/")
-    // .then((res) => res.json())
-    // .then((data) => {
-    //     console.log(data);
-    // })
   }
   
   render(){
+    const styles = {
+      display: this.state.timesheet ? {border: '1px solid white'} : {border: '1px solid transparent'},
+      shift: this.state.shiftActive ? {color: 'green'} : {color: 'white'},
+      active: this.state.shiftActive ? 'Active' : 'Not Active'
+    }
+  
+
     return(
       <div id="employeePortal">
         <div id="sideNav">
-          <div>Employee portal</div>
-          <div>Paycheck</div>
-          <div>Timesheet</div>
+          <div className='nav-item'>
+            <h3>Welcome, name!</h3>
+          </div>
+          <div className='nav-item'>
+            <p>Paycheck</p>
+          </div>
+          <div className='nav-item' style={styles.display}>
+            <p>Timesheet</p>
+          </div>
+          <div className='active-status'>
+            <p>Status: <span style={styles.shift}>{styles.active}</span></p>
+          </div>
+          <div className='logout-btn'>
+            <img id="logout-img" src={Logout}></img>
+            <p>Logout</p>
+          </div>
         </div>
         <div id="portalContent">
           <button>Clock In</button>
